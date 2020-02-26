f92ac9114684f242bd4736b7c012769d5309a36b
Roland Shorter <rolpep@yahoo.com>
2020-02-25 20:19:11 -0500

    updated

--- Shorter_Lab6/form.js
+++ Shorter_Lab6/form.js
@@ -1,6 +1,21 @@
 "use strict";
 var $ = function (id) { return document.getElementById(id); };
 
-var registerEntries = function( {
-    var header
+var registerEntries = function() {
+    var header = "";
+    var html = "";
+    var required - "<span> Required field</span>";
+    var msg = "Pleae fill in all required fields";
+    var fname =$ (fname).Value;
+    var lanme  = $(lname).Value; 
+    var address1 = $(address1).Value;
+    var address2 =$(address2).Value;
+    var city =$(city).Value;
+    var state  =$(state).Value;
+    var zipcode =$ (zipecode).Value;
+    var gender  =$(gender).Value;
+    var birth =$(birth).Value;
+    var mstatus =$(mstatus).Value;
+    var        =$().Value;
+
 }

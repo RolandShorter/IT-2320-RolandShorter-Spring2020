8573cd4d2f97ec3c83f7d66b7640fe1fac130647
Roland Shorter <rolpep@yahoo.com>
2020-02-25 20:19:35 -0500

    updated

--- Shorter_Lab6/form.js
+++ Shorter_Lab6/form.js
@@ -6,16 +6,80 @@ var registerEntries = function() {
     var html = "";
     var required - "<span> Required field</span>";
     var msg = "Pleae fill in all required fields";
-    var fname =$ (fname).Value;
-    var lanme  = $(lname).Value; 
-    var address1 = $(address1).Value;
-    var address2 =$(address2).Value;
-    var city =$(city).Value;
-    var state  =$(state).Value;
-    var zipcode =$ (zipecode).Value;
-    var gender  =$(gender).Value;
-    var birth =$(birth).Value;
-    var mstatus =$(mstatus).Value;
-    var        =$().Value;
+    var fname =$ (fname).value;
+    var lanme  = $(lname).value; 
+    var address1 = $(address1).value;
+    var address2 = $(address2).value;
+    var city = $(city).Value;
+    var state  = $(state).value;
+    var zipcode = $ (zipecode).value;
+    var gender  = $(gender).checked;
+    var birth = $(birth).value;
+    var mstatus = $(mstatus).value;
+    var sname  = $(sname).value;
+    var email = $(email).value;
+    var phone = $(phone).value;
+    var password = $(passowrd).value;
+    var password2 = $(password2).value;
+
+    if (fname == " "){
+        fname = required;
+        header = msg;
+    }
+    if (sname == " "){
+        sname = required;
+        header = msg; 
+    }
+    if (address1 == " "){
+        address1 = required;
+        header = msg;
+    }
+    if (address2 == " "){
+        address2 = required;
+        header = msg;
+    }
+    if (city == " "){
+        city = required;
+        header = required;
+    }
+    if (state == " "){
+        state = required;
+        header = required;
+    }
+    if (zipcode == " "){
+        zipcode = required;
+        header = msg;
+    }
+    if (gender == false){
+        gender = required;
+        header = msg;
+    }
+    if (birth == " "){
+        birth = required;
+        header = msg;
+    }
+    if (mstatus == " "){
+        mstatus = required;
+        header = msg;
+    }
+    if (sname == ){
+        
+    }
+    if (email == " "){
+        email = required;
+        header = msg;
+    }
+    if (phone == " "){
+        phone = required;
+        header = msg;
+    }
+    if (password == " "){
+       password = required;
+       header = msg; 
+    }
+    if (password2 == " "){
+       password2 = required;
+       header = msg; 
+    }
 
 }

2ed2f878b1a23ad407e68e39d200b977f309559a
rolpep <rjshorter4034@twc.com>
2020-03-08 14:08:17 -0400

    update

--- Shorter_Lab7/javascript.js
+++ Shorter_Lab7/javascript.js
@@ -1,6 +1,27 @@
 var $ = function(id) {
     return document.getElementById(id);
 }
+window.onload = function() {
+  var listNode = $("images");
   
 
-var i, image
+  var imageLinks = listNode.getElementsByTagName("a");
+}
+
+  function changePicture() {
+      console.log("changePicture")
+  }
+  //$("changePicture").addEventListener("click", changePicture, false)
+$("changePicture").onclick = changePicture
+var i, image, listNode, link;
+for  ( i = 0;  i < imageLinks.length; i++) {
+  var timeOut = setTimeout(swapPictures, 1000)
+//count down timerCount
+//get value from the input
+//at end of delay swap pictures
+//
+}
+
+function swapPictures() {
+
+}
